df490fcccd7f76367e9039b4408e9c74ee18a6fe
Ansh Kumar Singh <singhanshkumar299@gmail.com>
2026-04-29 23:31:16 +0530

    updated cors

--- Backend/app.js
+++ Backend/app.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 app.use(
     cors({
-        origin: ["*"],
+        origin: ["https://collab-write-delta.vercel.app"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
